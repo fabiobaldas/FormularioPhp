6e0d97bc70b5c624e9ccb0ab9654c695ab5d318d
Fabio Baldas <fabio.baldas@soulasalle.com.br>
2019-10-31 05:23:45 -0200

    Terceiro commit

--- FormularioMusicas/index.php
+++ FormularioMusicas/index.php
@@ -1,11 +1,6 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
-$banco="cadastro_cd";
-
-include_once 'conexao.php';
+include_once ('conexao.php');
 
 $artista=$_POST['art'];
 $titulo=$_POST['titulo'];
@@ -16,8 +11,8 @@ $tipo_musica=$_POST['tipo_musica'];
 $gravadora=$_POST['gravadora'];
 $musica=$_POST['musica'];
 
-$sql=mysqli_query($conexao,"INSERT INTO cd(art,titulo,ano,descricao,preco,tipo_musica,gravadora,musica) values('$artista','$titulo','$ano','$descricao', '$preco', '$tipo_musica','$gravadora','$musica')");
+$sql_nv = mysqli_query($conn,"INSERT INTO cd(art,titulo,descricao,preco,ano,tipo_musica,gravadora,musica) values('$artista','$titulo','$descricao', '$preco','$ano','$tipo_musica','$gravadora','$musica')");
 echo"<h1><center>Cadastro do CD <br> Realizado com Sucesso!!!";
-mysqli_close($conexao);
+mysqli_close($conn);
 
 ?>
