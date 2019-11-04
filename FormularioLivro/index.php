653b8a5df26761461a1d10605d95ae32589191aa
aluno.unilasalle <aluno.unilasalle@LB319UNI.ACADEMICO>
2019-11-04 20:27:31 -0200

    Aprendi na Aula 9

---
+++ FormularioLivro/index.php
@@ -0,0 +1,20 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$banco="cadastro_livro";
+$conexao=mysqli_connect($host,$user,$pass) or die (mysqli_error());
+mysqli_select_db($conexao,$banco)or die (mysqli_error());
+
+$titulo=$_POST['titulo'];
+$autor=$_POST['autor'];
+$editora=$_POST['editora'];
+$preco=$_POST['preco'];
+
+
+
+$sql=mysqli_query($conexao,"insert into livros(titulo,autor,editora,preco)values('$titulo', '$autor', '$editora', '$preco')");
+echo"<center>Cadastro Realizado com Sucesso!!!";
+mysqli_close($conexao);
+
+?>
