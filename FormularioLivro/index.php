2953f0efad0818635d662d9dfc944bd100736508
aluno.unilasalle <aluno.unilasalle@LB319UNI.ACADEMICO>
2019-11-04 22:17:31 -0200

    Adicionei os buttons

--- FormularioLivro/index.php
+++ FormularioLivro/index.php
@@ -13,8 +13,34 @@ $preco=$_POST['preco'];
 
 
 
+if($_POST['button'] == "Deletar"){
+    $sql = mysqli_query($conexao, "delete from livro where titulo = '{$titulo}");
+    echo"<h1> O livro '$titulo', foi deletado com sucesso!</h1>";
+}
+
+if($_POST['button'] == "Cadastrar"){
 $sql=mysqli_query($conexao,"insert into livros(titulo,autor,editora,preco)values('$titulo', '$autor', '$editora', '$preco')");
-echo"<center>Cadastro Realizado com Sucesso!!!";
+echo"<h1>O $titulo foi cadastrado com sucesso!!!</h1>";
+}
+
+if($_POST['button'] == "Atualizar"){
+    $sql=mysqli_query($conexao, "update livro set autor='{$autor}' where editora = '$editora'");
+    echo"<h1>O $autor foi atualizado com sucesso!!!</h1>";
+}
+
+if($_POST['button'] == "Selecionar"){
+    $busca = "select * from livro where titulo='{$titulo}'";
+    $sql = mysqli_query($conexao, $busca);
+    $i=0;
+    while ($linha= mysqli_fetch_array($sql)){
+        echo"<input type='text' value='".$linha['titulo']."'/>";
+        echo"<input type='text' value='".$linha['autor']."'/>";
+        echo"<input type='text' value='".$linha['editora']."'/>";
+        echo"<input type='text' value='".$linha['preco']."'/>";
+        $i++;
+    }
+}
 mysqli_close($conexao);
 
 ?>
+<a href="index.html">Voltar</a>
